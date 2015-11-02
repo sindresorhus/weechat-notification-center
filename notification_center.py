3269793334a3da9e628759d586c16862d7bd3d88
William Leuschner <iPlayGames2012@live.com>
2015-11-02 02:27:05 -0500

    PEP8, Icon Path, Icon in Notification
    Update the script to comply (mostly) with PEP8, define a path to the
    Weechat icon, and use it in the notifications.

--- notification_center.py
+++ notification_center.py
@@ -4,46 +4,78 @@
 import weechat
 from pync import Notifier
 
-
 SCRIPT_NAME = 'notification_center'
 SCRIPT_AUTHOR = 'Sindre Sorhus <sindresorhus@gmail.com>'
-SCRIPT_VERSION = '1.1.0'
+SCRIPT_VERSION = '1.0.0'
 SCRIPT_LICENSE = 'MIT'
-SCRIPT_DESC = 'Pass highlights and private messages to the OS X 10.8+ Notification Center'
+SCRIPT_DESC = ('Pass highlights and private messages to '
+               'the OS X 10.8+ Notification Center')
+WEECHAT_ICON = ".weechat/weechat.png"
 
-weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
+weechat.register(SCRIPT_NAME,
+                 SCRIPT_AUTHOR,
+                 SCRIPT_VERSION,
+                 SCRIPT_LICENSE,
+                 SCRIPT_DESC,
+                 '',
+                 ''
+                 )
 
 DEFAULT_OPTIONS = {
-	'show_highlights': 'on',
-	'show_private_message': 'on',
-	'show_message_text': 'on',
-	'sound': 'off',
-	'sound_name': 'Pong',
+    'show_highlights': 'on',
+    'show_private_message': 'on',
+    'show_message_text': 'on',
+    'sound': 'off',
 }
 
 for key, val in DEFAULT_OPTIONS.items():
-	if not weechat.config_is_set_plugin(key):
-		weechat.config_set_plugin(key, val)
+    if not weechat.config_is_set_plugin(key):
+        weechat.config_set_plugin(key, val)
 
 weechat.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
 
-def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
-	# ignore if it's yourself
-	own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
-	if prefix == own_nick or prefix == ('@%s' % own_nick):
-		return weechat.WEECHAT_RC_OK
 
-	# passing `None` or `''` still plays the default sound so we pass a lambda instead
-	sound = weechat.config_get_plugin('sound_name') if weechat.config_get_plugin('sound') == 'on' else lambda:_
-	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
-		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound)
-		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound)
-	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix, sound=sound)
-		else:
-			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound)
-	return weechat.WEECHAT_RC_OK
+def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
+    # ignore if it's yourself
+    own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
+    if prefix == own_nick or prefix == ('@%s' % own_nick):
+        return weechat.WEECHAT_RC_OK
+
+    # passing `None` or `''` still plays the default sound so we pass a
+    # lambda instead
+    sound = 'Pong' if weechat.config_get_plugin('sound') == 'on' else lambda:_
+    if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
+        channel = weechat.buffer_get_string(buffer, 'localvar_channel')
+        if weechat.config_get_plugin('show_message_text') == 'on':
+            Notifier.notify(
+                message,
+                title='%s %s' % (prefix, channel),
+                sound=sound,
+                appIcon=WEECHAT_ICON
+            )
+        else:
+            Notifier.notify(
+                'In %s by %s' % (channel, prefix),
+                title='Highlighted Message',
+                sound=sound,
+                appIcon=WEECHAT_ICON
+            )
+    elif (
+        weechat.config_get_plugin('show_private_message') == 'on' and
+        'notify_private' in tags
+    ):
+        if weechat.config_get_plugin('show_message_text') == 'on':
+            Notifier.notify(
+                message,
+                title='%s [private]' % prefix,
+                sound=sound,
+                appIcon=WEECHAT_ICON
+            )
+        else:
+            Notifier.notify(
+                'From %s' % prefix,
+                title='Private Message',
+                sound=sound,
+                appIcon=WEECHAT_ICON
+            )
+    return weechat.WEECHAT_RC_OK
