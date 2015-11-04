5b2ddc6acc45a4358ae351478be935e7afb33aad
William Leuschner <iPlayGames2012@live.com>
2015-11-04 13:37:09 +0700

    Close #8 PR: Add App Icon to Notification.

--- notification_center.py
+++ notification_center.py
@@ -3,6 +3,7 @@
 
 import weechat
 from pync import Notifier
+import os
 
 
 SCRIPT_NAME = 'notification_center'
@@ -10,6 +11,7 @@ SCRIPT_AUTHOR = 'Sindre Sorhus <sindresorhus@gmail.com>'
 SCRIPT_VERSION = '1.1.0'
 SCRIPT_LICENSE = 'MIT'
 SCRIPT_DESC = 'Pass highlights and private messages to the OS X 10.8+ Notification Center'
+WEECHAT_ICON = os.path.expanduser('~/.weechat/weechat.png')
 
 weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
 
@@ -38,12 +40,12 @@ def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
 	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
 		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound)
+			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound, appIcon=WEECHAT_ICON)
 		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound)
+			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound, appIcon=WEECHAT_ICON)
 	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
 		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix, sound=sound)
+			Notifier.notify(message, title='%s [private]' % prefix, sound=sound, appIcon=WEECHAT_ICON)
 		else:
-			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound)
+			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound, appIcon=WEECHAT_ICON)
 	return weechat.WEECHAT_RC_OK
