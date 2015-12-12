73043f2ebc049cf2d03f2fd4c65c1610e638dfc6
Sébastien Helleu <flashcode@flashtux.org>
2015-12-12 07:30:39 +0100

    Replace tabs with spaces for indentation
    As recommended by PEP8:
    https://www.python.org/dev/peps/pep-0008/#tabs-or-spaces

--- notification_center.py
+++ notification_center.py
@@ -16,38 +16,38 @@ WEECHAT_ICON = os.path.expanduser('~/.weechat/weechat.png')
 weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, '', '')
 
 DEFAULT_OPTIONS = {
-	'show_highlights': 'on',
-	'show_private_message': 'on',
-	'show_message_text': 'on',
-	'sound': 'off',
-	'sound_name': 'Pong',
-	'activate_bundle_id': 'com.apple.Terminal',
+    'show_highlights': 'on',
+    'show_private_message': 'on',
+    'show_message_text': 'on',
+    'sound': 'off',
+    'sound_name': 'Pong',
+    'activate_bundle_id': 'com.apple.Terminal',
 }
 
 for key, val in DEFAULT_OPTIONS.items():
-	if not weechat.config_is_set_plugin(key):
-		weechat.config_set_plugin(key, val)
+    if not weechat.config_is_set_plugin(key):
+        weechat.config_set_plugin(key, val)
 
 weechat.hook_print('', 'irc_privmsg', '', 1, 'notify', '')
 
 def notify(data, buffer, date, tags, displayed, highlight, prefix, message):
-	# ignore if it's yourself
-	own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
-	if prefix == own_nick or prefix == ('@%s' % own_nick):
-		return weechat.WEECHAT_RC_OK
+    # ignore if it's yourself
+    own_nick = weechat.buffer_get_string(buffer, 'localvar_nick')
+    if prefix == own_nick or prefix == ('@%s' % own_nick):
+        return weechat.WEECHAT_RC_OK
 
-	# passing `None` or `''` still plays the default sound so we pass a lambda instead
-	sound = weechat.config_get_plugin('sound_name') if weechat.config_get_plugin('sound') == 'on' else lambda:_
-	activate_bundle_id = weechat.config_get_plugin('activate_bundle_id')
-	if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
-		channel = weechat.buffer_get_string(buffer, 'localvar_channel')
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
-		else:
-			Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
-	elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
-		if weechat.config_get_plugin('show_message_text') == 'on':
-			Notifier.notify(message, title='%s [private]' % prefix, sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
-		else:
-			Notifier.notify('From %s' % prefix, title='Private Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
-	return weechat.WEECHAT_RC_OK
+    # passing `None` or `''` still plays the default sound so we pass a lambda instead
+    sound = weechat.config_get_plugin('sound_name') if weechat.config_get_plugin('sound') == 'on' else lambda:_
+    activate_bundle_id = weechat.config_get_plugin('activate_bundle_id')
+    if weechat.config_get_plugin('show_highlights') == 'on' and int(highlight):
+        channel = weechat.buffer_get_string(buffer, 'localvar_channel')
+        if weechat.config_get_plugin('show_message_text') == 'on':
+            Notifier.notify(message, title='%s %s' % (prefix, channel), sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+        else:
+            Notifier.notify('In %s by %s' % (channel, prefix), title='Highlighted Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+    elif weechat.config_get_plugin('show_private_message') == 'on' and 'notify_private' in tags:
+        if weechat.config_get_plugin('show_message_text') == 'on':
+            Notifier.notify(message, title='%s [private]' % prefix, sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+        else:
+            Notifier.notify('From %s' % prefix, title='Private Message', sound=sound, appIcon=WEECHAT_ICON, activate=activate_bundle_id)
+    return weechat.WEECHAT_RC_OK
